9bcda3879f8a3c9be6113b3b32b7b71d6067f289
Sami Onur Zaim <onurzaim@hotmail.com>
2015-09-11 16:06:04 +0300

    Daily Mysql Backup Script
    This script will make full backup of your mysql database server with unique name and copy it to remote server with scp.

--- DailyMysqlBackup.sh
+++ DailyMysqlBackup.sh
@@ -7,7 +7,7 @@ Saat=$(date | awk -F' ' '{ print $4 }' | awk -F':' '{ print $1 }')
 Dakika=$(date | awk -F' ' '{ print $4 }' | awk -F':' '{ print $2 }')
 Saniye=$(date | awk -F' ' '{ print $4 }' | awk -F':' '{ print $3 }')
 mysqldump -uusername -ppassword -h 127.0.0.1 --all-databases > Secbir-$DbName-$Yil-$Gun-$Saat$Dakika$Saniye.sql
-bzip2 -z Secbir-$DbName-$Yil-$Gun-$Saat$Dakika$Saniye.sql
+bzip2 -z DbBackup-$DbName-$Yil-$Gun-$Saat$Dakika$Saniye.sql
 
 # Move backup to remote server with scp. You must make trust with rsa keys.
-scp Secbir-$DbName-$Yil-$Gun-$Saat$Dakika$Saniye.sql.bz2 username@example.com:/backup/db
+scp DbBackup-$DbName-$Yil-$Gun-$Saat$Dakika$Saniye.sql.bz2 username@example.com:/backup/db
