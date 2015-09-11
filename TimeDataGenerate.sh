00620770917c9d4d5394b8277cde27d671faeed2
Sami Onur Zaim <onurzaim@hotmail.com>
2015-09-11 16:07:41 +0300

    Update TimeDataGenerate.sh

--- TimeDataGenerate.sh
+++ TimeDataGenerate.sh
@@ -9,6 +9,8 @@ SaatStart=$(date +%k)
 
 SaatStartMinus1Hr=$(echo -e $SaatStart-1 | bc)
 
+# Test amacli echo
+
 echo $SaatStartMinus1Hr
 
 # Bitis saati tayin edilir.
@@ -18,9 +20,10 @@ AyEnd=$(date +%m)
 GunEnd=$(date +%d)
 SaatEnd=$(date +%k)
 
+
 SaatStartMinus3Hr=$(echo -e $SaatStart-1 | bc)
 SaatStartMinus4Hr=$(echo -e $SaatStart-3 | bc)
 
-# timedata.1 dosyasindaki varX'ler sed komutu ile degistirilir.
+# timedata.1 dosyasindaki varX'ler sed komutu ile degistirilir. timedata dosyasi konacak.
 
 sed -e 's/var1/'$YilStart'/' -e 's/var2/'$AyStart'/'  -e 's/var3/'$GunStart'/'  -e 's/var4/'$SaatStartMinus4Hr'/'  -e 's/var5/'$YilEnd'/'  -e 's/var6/'$AyEnd'/'  -e 's/var7/'$GunEnd'/'  -e 's/var8/'$SaatStartMinus3Hr'/'  timedata.1 > timedata
