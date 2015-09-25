b8e6c159d0974797a53df15450741ca2f8bc714b
Sami Onur Zaim <onurzaim@hotmail.com>
2015-09-25 21:32:07 +0300

    Create TelnetMail.sh

---
+++ TelnetMail.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#This scripts send mail from telnet with ehlo
+Concurrent=$(curl http://my.url.com)
+
+echo "$Concurrent"
+
+(
+sleep 1;
+echo HELO ehlo.name;
+sleep 1;
+echo mail from: mail@address.com;
+sleep 1;
+echo rcpt to: mail.recipients@address.com;
+sleep 1;
+echo data;
+sleep 1;
+echo Subject: Subject Name;
+echo
+echo
+echo "$Concurrent";
+echo .;
+sleep 1;
+echo quit
+) | telnet my.mailserver.com 25
